491efcc6bdb1c6f09ea4fe724260df67cc2dbf62
stefano.ottaviani <stefano@smartmobile.it>
2024-03-04 15:59:06 +0100

    Created empty projects

---
+++ src/MailsSender.ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MailsSender.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
